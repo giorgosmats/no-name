21283d2efc5a2a281f77b50bc6c34d3373562fbb
George Matsiras <geo.matsiras@gmail.com>
2022-01-06 02:40:40 +0200

    feat: create api controllers

--- src/main/java/gr/noname/rest/CommentController.java
+++ src/main/java/gr/noname/rest/CommentController.java
@@ -1,7 +1,58 @@
 package gr.noname.rest;
 
-import org.springframework.web.bind.annotation.RestController;
+import gr.noname.middleware.entities.Comment;
+import gr.noname.middleware.repositories.CommentRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CommentController {
+
+    private final CommentRepository repository;
+
+    CommentController(CommentRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/api/comments")
+    List<Comment> GetComments(
+            @RequestParam(name = "commentStartsWith", required = false) String commnentStartsWith
+    ) {
+
+        if (commnentStartsWith != null && commnentStartsWith != "") {
+            return repository.findByCommentStartingWith(commnentStartsWith);
+        }
+        return repository.findAll();
+    }
+
+    @GetMapping("/api/comments/{id}")
+    Comment GetComment(@PathVariable("id") Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cannot find comment with id " + id));
+    }
+
+    @PutMapping("/api/comments/{id}")
+    Comment updateComment(@RequestBody Comment newComment, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(match -> {
+                    match.setComment(newComment.getComment());
+                    match.setPerson(newComment.getPerson());
+                    return repository.save(match);
+                })
+                .orElseGet(() -> {
+                    newComment.setId(id);
+                    return repository.save(newComment);
+                });
+    }
+    @DeleteMapping("/api/comments")
+    void deleteAllCars() {
+    repository.deleteAll();
+}
+
+    @DeleteMapping("/api/comments/{id}")
+    void deleteCar(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
--- src/main/java/gr/noname/rest/PersonController.java
+++ src/main/java/gr/noname/rest/PersonController.java
@@ -2,6 +2,7 @@ package gr.noname.rest;
 
 import gr.noname.middleware.entities.Person;
 import gr.noname.middleware.repositories.PersonRepository;
+import org.aspectj.weaver.patterns.PerObject;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,16 +13,53 @@ public class PersonController {
 
     PersonController(PersonRepository repository) { this.repository = repository; }
 
-    @GetMapping(value = "/api/people")
-    List<Person> showPeople(){return repository.findAll();}
 
-//    @GetMapping("/api/people/{id}")
-//    Person showPerson(@PathVariable("id") Long id){}
-//
-//    @PutMapping("/api/people/{id}")
-//    Person upadatePerson(@RequestParam Person newPerson, @PathVariable("id") Long id){}
-//
-//    @DeleteMapping("/api/people/{id}")
-//    void deletePerson(@PathVariable("id") Long id){repository.deleteById(id);}
+    @GetMapping(value = "/api/people")
+    List<Person> GetPeople(
+            @RequestParam(name = "emailStartsWith", required = false) String emailStartsWith
+    ) {
+
+        if (emailStartsWith != null && emailStartsWith != "") {
+            return repository.findByEmailStartingWith(emailStartsWith);
+        }
+        return repository.findAll();
+    }
+
+    @GetMapping("/api/people/{id}")
+    Person GetPerson(@PathVariable("id") Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cannot find person with id " + id));
+    }
+
+    @PutMapping("/api/people/{id}")
+    Person updateCar(@RequestBody Person newPerson, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(match -> {
+                    match.setEmail(newPerson.getEmail());
+                    match.setPassword(newPerson.getPassword());
+                    match.setFirstName(newPerson.getFirstName());
+                    match.setLastName(newPerson.getLastName());
+                    match.setGender(newPerson.getGender());
+                    match.setAddress(newPerson.getAddress());
+                    match.setContactDetails(newPerson.getContactDetails());
+                    return repository.save(match);
+                })
+                .orElseGet(() -> {
+                    newPerson.setId(id);
+                    return repository.save(newPerson);
+                });
+    }
+
+    @DeleteMapping("/api/people")
+    void deleteAllPeople() {
+    repository.deleteAll();
+}
+
+    @DeleteMapping("/api/people/{id}")
+    void deletePerson(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
 
 }
